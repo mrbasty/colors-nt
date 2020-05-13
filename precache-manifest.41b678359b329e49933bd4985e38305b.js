b295d1de96c8344a894c45a35c4907faaead5539
Bastien Adam <bastien.adam.08@gmail.com>
2020-05-13 10:38:04 -0400

    Updates

---
+++ precache-manifest.41b678359b329e49933bd4985e38305b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef319c7f7aad69a2e83bae42c848612",
+    "url": "/colors-nt/index.html"
+  },
+  {
+    "revision": "8d0e1ecf76f7539c4a75",
+    "url": "/colors-nt/static/css/main.2136b6db.chunk.css"
+  },
+  {
+    "revision": "40ed6e25a360d2a58403",
+    "url": "/colors-nt/static/js/2.0b3d3209.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/colors-nt/static/js/2.0b3d3209.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d0e1ecf76f7539c4a75",
+    "url": "/colors-nt/static/js/main.b18f22ca.chunk.js"
+  },
+  {
+    "revision": "13b4ba18d4a8caf2435c",
+    "url": "/colors-nt/static/js/runtime-main.73be0f3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/colors-nt/static/media/logo.5d5d9eef.svg"
+  }
+]);
